09b12cdad3db5588d02c3cb74e0baa4e6d5a74dc
Jensen <jensen@cloudflare.com>
2020-03-03 19:44:45 -0600

    New module to fetch data from a csv and return json

---
+++ fetchData.js
@@ -0,0 +1,79 @@
+const axios = require("axios");
+const cron = require('node-cron');
+const csv = require("csvtojson");
+const fs = require("fs");
+
+const URL = `https://docs.google.com/spreadsheets/d/14dnT6yUxZiHWvPaEiWsOKu1xPQ_xwkuuUDfMGmFHinc/gviz/tq?tqx=out:csv&sheet=Sheet1`;
+const CSV_URL = "/tmp/data.csv";
+
+exports.fetchData = async () => {
+  return axios({
+    method: "get",
+    url: URL,
+    responseType: "stream"
+  }).then(response => {
+    response.data.pipe(fs.createWriteStream(CSV_URL));
+
+    return csv()
+      .fromFile(CSV_URL)
+      .then(json => generatedData(json));
+  });
+};
+
+const removeEmptyRows = data => {
+  return data.filter(row => !!row["country "]);
+};
+
+const gatherCategoryIndexes = (order, data) => {
+  return order.map(key =>
+    data.findIndex(element => {
+      return element["country "] === key;
+    })
+  );
+};
+
+const gatherBetweenRows = (startKey, endKey, data) => {
+  return data.slice(startKey + 1, endKey);
+};
+
+const generatedData = data => {
+  const sanitiziedData = removeEmptyRows(data);
+  const totalStartKey = "CASES";
+  const chinaStartKey = "MAINLAND CHINA";
+  const chinaTotalKey = "CHINA TOTAL";
+  const otherStartKey = "OTHER PLACES";
+  const otherTotalKey = "TOTAL";
+
+  const rowOrder = [
+    totalStartKey,
+    chinaStartKey,
+    chinaTotalKey,
+    otherStartKey,
+    otherTotalKey
+  ];
+  const rowIndexes = gatherCategoryIndexes(rowOrder, sanitiziedData);
+
+
+  const sortedData = {
+    totalWorld: gatherBetweenRows(rowIndexes[0], rowIndexes[1], sanitiziedData),
+
+    chinaProvinces: gatherBetweenRows(rowIndexes[1], rowIndexes[2], sanitiziedData),
+    totalChina: sanitiziedData.find(element => {
+      return element["country "] === chinaTotalKey;
+    }),
+
+    otherProvinces: gatherBetweenRows(rowIndexes[3], rowIndexes[4], sanitiziedData),
+    otherTotal: sanitiziedData.find(element => {
+      return element["country "] === otherTotalKey;
+    })
+  };
+
+  return sortedData;
+};
+
+//fetchData();
+
+// Fetch data every 10 minutes.
+// cron.schedule('* 10 * * *', () => {
+//   fetchData();
+// });
