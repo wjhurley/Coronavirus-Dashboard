79a0f25398ac8e11f5faee053618dafe018bd574
Jensen <jensen@cloudflare.com>
2020-03-04 09:13:12 -0600

    Adding a favicon and meta details. Cleaning up index.js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-var request = require("request");
-var cheerio = require("cheerio");
-var rp = require("request-promise");
+const cron = require("node-cron");
+const express = require("express");
+const fs = require("fs");
 const path = require("path");
 const stats = require("./fetchData");
-const cron = require("node-cron");
-const fs = require("fs");
+
 const JSON_URL = './tmp/statistics.json'
 let statistics = require(JSON_URL)
 
@@ -12,7 +11,6 @@ let statistics = require(JSON_URL)
 cron.schedule('* * * * *', () => {
   stats.fetchData().then(data => {
     try {
-      console.log(data);
       fs.writeFileSync(JSON_URL, JSON.stringify(data));
       delete require.cache[require.resolve(JSON_URL)];
       statistics = require(JSON_URL)
@@ -22,48 +20,20 @@ cron.schedule('* * * * *', () => {
   })
 });
 
-var express = require("express");
-var app = express();
+
+const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
+app.get("/", (req, res) => res.render("data", statistics));
+app.get("/about", (req, res) => res.render("about"));
+app.get("/data", (req, res) => res.render("data", statistics));
+app.get("/faq", (req, res) => res.render("faq"));
+app.get("/map", (req, res) => res.render("map"));
+app.get("/preparation", (req, res) => res.render("prepping"));
+app.get("/prevention", (req, res) => res.render("prevention"));
+app.get("/tweets", (req, res) => res.render("tweets"));
+app.get("/wiki", (req, res) => res.render("wiki"));
 
-app.get("/", async (req, res) => res.render("data", statistics));
-
-app.get("/data", async (req, res) => res.render("data", statistics));
-
-
-// app.get("/timeline", async (req, res) => {
-//   // console.log(holder[13]);
-//   // console.log(holder[12]);
-//       console.log(holder);
-//   res.render("timeline", {
-//     timelineDataSplitArray: holder[10] || [],
-//     timelineDates: holder[11],
-//     timelineArrayDates: holder[12],
-//     timelineDataSources: holder[13]
-//   });
-// });
-app.get("/prevention", async (req, res) => {
-  res.render("prevention");
-});
-app.get("/about", async (req, res) => {
-  res.render("about");
-});
-app.get("/map", async (req, res) => {
-  res.render("map");
-});
-app.get("/wiki", async (req, res) => {
-  res.render("wiki");
-});
-app.get("/faq", async (req, res) => {
-  res.render("faq");
-});
-app.get("/tweets", async (req, res) => {
-  res.render("tweets");
-});
-app.get("/preparation", async (req, res) => {
-  res.render("prepping");
-});
 app.listen(process.env.PORT || 3000);
 console.log("Listening on port: " + 3000);
