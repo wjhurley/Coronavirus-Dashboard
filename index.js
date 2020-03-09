eab553eabbc40488b9cfe21044a8c1b127641fb7
Jensen <jensen@cloudflare.com>
2020-03-08 20:41:03 -0500

    Using proper content for route

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const path = require("path");
 const stats = require("./fetchData");
 const sync = require("./syncData");
 const time = require("./getTime");
+const content = require("./data/preparation")
 
 //Fetch data every 10 minutes.
 cron.schedule("* * * * *", () => {
@@ -31,7 +32,7 @@ app.get("/about", (req, res) => res.render("about"));
 app.get("/data", (req, res) => getContent(res, "data"));
 app.get("/faq", (req, res) => res.render("faq"));
 app.get("/map", (req, res) => res.render("map"));
-app.get("/preparation", (req, res) => res.render("prepping"));
+app.get("/preparation", (req, res) => res.render("preparation", {data: content.data} ));
 app.get("/prevention", (req, res) => res.render("prevention"));
 app.get("/tweets", (req, res) => res.render("tweets"));
 app.get("/wiki", (req, res) => res.render("wiki"));
