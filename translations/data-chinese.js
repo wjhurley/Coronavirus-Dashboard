d7f3c411f99a91e2630fddaa8e1e3c017b10a552
Avi Schiffmann <avischiffmann@gmail.com>
2020-03-09 19:09:06 -0700

    cleanup new wiki, etc

---
+++ translations/data-chinese.js
@@ -0,0 +1,66 @@
+exports.data = {
+    quickfacts: {
+      QuickFacts: ["要闻速览"],
+      TotalConfirmedCases: ["确诊病例总数"],
+      TotalDeceased: ["死者总数"],
+      quickfacts: ["要闻速览"],
+      quickfacts: ["要闻速览"],
+      quickfacts: ["要闻速览"],
+      quickfacts: ["要闻速览"],
+
+        "",
+        "死者总数",
+        "总严重",
+        "总回收",
+        "受感染的国家总数"
+      ]
+    },
+    details: {
+        items: [
+          "全球",
+          "已确认",
+          "严重",
+          "危急",
+          "已恢复",
+          "死者"
+        ]
+      },
+      globalCountries: {
+        Afghanistan: ["阿富汗"],
+        Albania: ["阿尔巴尼亚"],
+        Algeria: ["阿尔及利亚"],
+        Afghanistan: ["阿富汗"],
+        Afghanistan: ["阿富汗"],
+        Afghanistan: ["阿富汗"],
+        Afghanistan: ["阿富汗"],
+        Afghanistan: ["阿富汗"],
+        Afghanistan: ["阿富汗"],
+        Afghanistan: ["阿富汗"],
+        Afghanistan: ["阿富汗"],
+        Afghanistan: ["阿富汗"],
+        Afghanistan: ["阿富汗"],
+        Afghanistan: ["阿富汗"],
+        Afghanistan: ["阿富汗"],
+        Afghanistan: ["阿富汗"],
+        Afghanistan: ["阿富汗"],
+        Afghanistan: ["阿富汗"],
+        Afghanistan: ["阿富汗"],
+        Afghanistan: ["阿富汗"],
+        Afghanistan: ["阿富汗"],
+        Afghanistan: ["阿富汗"],
+        Afghanistan: ["阿富汗"],
+        Afghanistan: ["阿富汗"],
+        Afghanistan: ["阿富汗"],
+        Afghanistan: ["阿富汗"],
+        Afghanistan: ["阿富汗"],
+        Afghanistan: ["阿富汗"],
+        Afghanistan: ["阿富汗"],
+        Afghanistan: ["阿富汗"],
+        Afghanistan: ["阿富汗"],
+        Afghanistan: ["阿富汗"],
+        Afghanistan: ["阿富汗"],
+        Afghanistan: ["阿富汗"],
+        Afghanistan: ["阿富汗"],
+      }
+  };
+  
---
+++ translations/data-spanish.js
@@ -0,0 +1,21 @@
+exports.data = {
+    quickfacts: {
+      items: [
+        "Hechos rápidos",
+        "Total de casos confirmados",
+        "Total fallecido",
+        "Serio total",
+        "Dishwasher soap",
+        "Household cleaner",
+        "Laundry soap",
+        "Bleach (unscented pure bleach)",
+        "Isopropyl Rubbing Alcohol",
+        "Medical grade disinfectant spray (equivalent to Lysol)",
+        "Bathroom cleaner products",
+        "Shaving stuff (note to men: respirator masks only work if you are clean-shaven, there has to be a tight skin to mask seal)",
+        "Extras of everything you need refills of",
+        "Nitrile disposable gloves"
+      ]
+    }
+  };
+  
