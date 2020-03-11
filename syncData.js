333bdd6d7b92745498cc1c372267c7035c5064ce
Jensen <jensen@cloudflare.com>
2020-03-10 22:48:56 -0500

    Remove commas when doing math...

--- syncData.js
+++ syncData.js
@@ -4,15 +4,23 @@ const utilities = require("./utilities");
 
 exports.gatherAllRegions = () => {
   return Promise.all(
-    globals.allRegions.map(region => fs.promises.readFile(utilities.getJSONPath(region)))
+    globals.allRegions.map(region =>
+      fs.promises.readFile(utilities.getJSONPath(region))
+    )
   ).then(values => {
     let data = {};
 
     values.forEach(value => {
       const regionData = JSON.parse(value);
       data[regionData.regionName] = regionData;
-      data[regionData.regionName].recoveryRate = parseInt(data[regionData.regionName].regionTotal.recovered)/parseInt(data[regionData.regionName].regionTotal.cases)*100;
-
+      data[regionData.regionName].recoveryRate =
+        (parseInt(
+          data[regionData.regionName].regionTotal.recovered.replace(",", "")
+        ) /
+          parseInt(
+            data[regionData.regionName].regionTotal.cases.replace(",", "")
+          )) *
+        100;
     });
     return {
       ...data,
