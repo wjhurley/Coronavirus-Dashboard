8eff157093c9781e355e6a1ef76c2c4fbafc2959
Jensen <jensen@cloudflare.com>
2020-03-12 20:51:09 -0500

    Adding all European countries to global list

--- globals.js
+++ globals.js
@@ -59,7 +59,35 @@ exports.displayOrder = [
 ];
 
 exports.countryLists = {
-  Europe: ["Spain", "France", "Germany", "Italy"]
+  Europe: ["Austria",
+  "Belgium",
+  "Bulgaria",
+  "Croatia",
+  "Cyprus",
+  "Czech Republic",
+  "Denmark",
+  "Estonia",
+  "Finland",
+  "France",
+  "Germany",
+  "Greece",
+  "Hungary",
+  "Ireland",
+  "Italy",
+  "Latvia",
+  "Lithuania",
+  "Luxembourg",
+  "Malta",
+  "Netherlands",
+  "Poland",
+  "Portugal",
+  "Romania",
+  "Slovak Republic",
+  "Slovenia",
+  "Spain",
+  "Sweden",
+  "United Kingdom"]
+
 };
 
 exports.regionStructure = {
